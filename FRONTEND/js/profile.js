4fc1bd263f72548c56d1b3a1c3f39b7cc0211fc5
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-11-27 09:52:58 +0530

    Update profile.js

--- FRONTEND/js/profile.js
+++ FRONTEND/js/profile.js
@@ -3,40 +3,48 @@ import navbar from "../components/navbar.js";
 import { isSuperAdmin } from "../utils/Cookies.js";
 document.getElementById("navbar").innerHTML = navbar();
 
+const admin = (data) => {
+  data.map(admin => {
+    let div = document.createElement("div")
+    let h1 = document.createElement("h1")
+    h1.innerHTML = admin.username;
+    let p = document.createElement("p");
+    p.innerHTML = admin.email;
+    let role = document.createElement("h4")
+    role.innerHTML = admin.role;
+    
+    let active = document.createElement("button");
+    active.textContent = admin.isActive ? "Active" : "Approve";
+
+    active.addEventListener("click", async () => {
+      if (!admin.isActive) {
+        const response = await superAdminApi.updateAdminStatus(admin._id, true);
+        
+        if (response) {
+            admin.isActive = true;
+            button.textContent = "Active";
+          
+        }
+      }
+    });
+    let reject = document.createElement("button");
+    reject.textContent = "reject" 
+
+    reject.addEventListener("click", async () => {
+       await superAdminApi.deleteAdminStatus(admin._id, true);
+    });
+    let hr = document.createElement("hr")
+    div.append(h1 , p , role ,active,reject ,   hr)
+    document.getElementById("alladmin").append(div)
+  })
+}
+
 if (isSuperAdmin()) {
   let data = await superAdminApi.getAdmins();
   console.log(data);
-  const showadmin = document.getElementById("alladmin") 
-  if(data && data.length > 0){
-    data.map(admin => {
-      let div = document.createElement("div")
-      let h1 = document.createElement("h1")
-      h1.innerHTML = admin.username;
-      let p = document.createElement("p");
-      p.innerHTML = admin.email;
-      let role = document.createElement("h4")
-      role.innerHTML = admin.role;
-      
-      let button = document.createElement("button");
-      button.textContent = admin.isActive ? "Active" : "Approve";
-
-      button.addEventListener("click", async () => {
-        if (!admin.isActive) {
-          const response = await superAdminApi.updateAdminStatus(admin._id, true);
-          
-          if (response) {
-              admin.isActive = true;
-              button.textContent = "Active";
-            
-          }
-        }
-      });
-
-      let hr = document.createElement("hr")
-      div.append(h1 , p , role ,button,  hr)
-      showadmin.append(div)
-    })
-  }
+  const unactiveadimn = data.filter((admin) => admin.isVerified == false)
+  admin(unactiveadimn)
+  
 }
 else{
     console.log("No super admin");
