eb16eeb6dfae2d7467c1c509ccdc4de2e9948ed0
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-12-10 10:04:33 +0530

    Update cart.controller.js

--- BACKEND/controllers/cart.controller.js
+++ BACKEND/controllers/cart.controller.js
@@ -4,7 +4,7 @@ const getByProductId = async (req, res) => {
     const { userId,productId } = req.params;
 
     try {
-        const cart = await Cart.findOne( userId ).populate( productId);
+        const cart = await Cart.findOne({user:userId}).populate(productId);
         res.send(cart);
     } catch (error) {
         res.send({ error: error.message });
@@ -13,16 +13,17 @@ const getByProductId = async (req, res) => {
 
 const addToCart = async (req, res) => {
     const { userId, productId } = req.body;
-
+    console.log(req.body);  
     try {
-        let cart = await Cart.findOne({ user: userId });
+        let cart = await Cart.create({user:userId, product: productId });
+        console.log("cart", cart);   
         let isExtistent =await Cart.findOne({user: userId, product: productId})
         if(isExtistent) {
             cart.qty += 1
             cart.save();
-            res.send(isExtistent)
+            res.send({isExtistent})
         } else {
-            res.send(cart);
+            res.send({cart});
         }
     } catch (error) {
         res.send({ error: error.message });
@@ -45,11 +46,22 @@ const addQuantity = async (req, res) => {
     const { cartId, qty } = req.body;
 
     try {
-        const cart = await Cart.findByIdAndUpdate();
+        qty += 1; 
+        const cart = await Cart.findByIdAndUpdate({ _id:cartId }, {qty: qty},{new: true});
         res.send(cart);
     } catch (error) {
         res.send({ error: error.message });
     }
 };
+const removeQuantity = async (req, res) => {
+    const { cartId, qty } = req.body;
 
-module.exports = { getByProductId, addToCart, removeFromCart, addQuantity };
+    try {
+        qty -= 1; 
+        const cart = await Cart.findByIdAndUpdate({ _id:cartId }, {qty: qty},{new: true});
+        res.send(cart);
+    } catch (error) {
+        res.send({ error: error.message });
+    }
+};
+module.exports = { getByProductId, addToCart, removeFromCart, addQuantity, removeQuantity};
