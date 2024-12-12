c87373931eae72b1e681a6d87bc7d676222b279c
Harsh_Diyora <146904128+Harsh90904@users.noreply.github.com>
2024-12-12 09:39:56 +0530

    Update cart.router.js

--- BACKEND/routers/cart.router.js
+++ BACKEND/routers/cart.router.js
@@ -1,12 +1,18 @@
-const {Router} = require('express');
-const { getByProductId, addToCart, removeFromCart, updateQuantity } = require('../controllers/cart.controller'); 
-const cartrouter = Router();
+const { Router } = require("express");
+const {
+  getByUserId,
+  addToCart,
+  removeCart,
+  addQty,
+  removeQty,
+} = require("../controllers/cart.controller");
+const { decode } = require("../middlewares/decodeJwt");
 
+const cartRoute = Router();
+cartRoute.get("/", decode, getByUserId);
+cartRoute.post("/", decode, addToCart);
+cartRoute.delete("/:cartId", decode, removeCart);
+cartRoute.patch("/add-qty/:cartId", decode, addQty);
+cartRoute.patch("/remove-qty/:cartId", decode, removeQty);
 
-cartrouter.get("/:userId", getByProductId); 
-cartrouter.post("/add", addToCart); 
-cartrouter.delete("/:cartId", removeFromCart); 
-// cartrouter.patch('/add-qty/:productid', addQuantity); 
-// cartrouter.post('/remove-quantity', removeQuantity); 
-cartrouter.patch("/update-quantity/:cartId", updateQuantity);
-module.exports = cartrouter;
+module.exports = cartRoute;
